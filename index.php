75fdf64bb67c23db5d512684bb4ed0eb44aa2474
Candle779 <62902629+Candle779@users.noreply.github.com>
2020-06-28 15:24:34 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,255 @@
+  <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>Вебчат</title>
+ 	<link rel="stylesheet" type="text/css" href="main.css">
+ </head>
+ <body>
+ 
+ 	<!--
+	1. Сверстать шапку
+		1.1. Сделать логотип
+		1.2. Сделать меню
+
+	2. Сверстать левое меню
+		2.1. Сделать поиск
+		2.2. Сделать список контактов.
+	3. Сверстать переписку с пользователем
+		3.1. Вывод сообщений
+		3.2. Форма для отправки сообщений
+	4. Список контактов модальное окно
+		4.1. Поле поиска
+		4.2. список контактов
+	5. Окно авторизации и регистрации
+
+
+
+ 	-->
+
+ 	<div id="shapka">
+ 		 <div id="logo"> 
+ 		 	<img src="/images/1.png"><span><b>Веб</b><i>ЧАТ</i></span>
+ 		 </div>
+ 		 <div id="menu">
+ 		 	<a href="#">Контакты</a>
+ 		 	<a href="#">Настройки</a>
+ 		 	<a href="#">Войти</a>
+ 		 </div>
+ 	</div>
+
+ 	<div id="content">
+ 		<div id="users">
+ 			<div id="poisk">
+ 				<input type="text" name="text">
+ 				 			
+ 				<button>  
+ 					 <img src=images/search2.png>
+ 				</button>
+ 			</div>
+
+ 			<div id="spisok">
+ 				<ul>
+
+ 					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+ 					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+
+ 					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+
+ 					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+
+ 					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+
+ 					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+
+  					<li>
+ 						<div class="avatar">
+ 							<img src="images/user.png">
+ 						</div>
+ 						<h2>Peter</h2>
+ 						<p>Привет!</p>
+ 						<div class="time">09:10</div>
+
+ 					</li>
+
+ 				</ul>
+ 			</div>
+ 		</div>
+
+ 		<div id = "soobsheniya">
+ 		 	<div id="spisok-soobsheniy">
+
+              <ul>
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>
+                 
+                 <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+                </li>     
+
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>    
+
+                 <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>    
+
+                 <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>
+                
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>
+                
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+                </li>
+                
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>
+                
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>
+                
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+
+                </li>
+                
+                <li>
+                    <div class="avatar">
+                      <img src="images/user.png">
+                    </div>
+
+                  <h2>Peter</h2>
+                  <p >Привет</p>
+                  <div class="time">9:10</div>
+                </li>
+              </ul>
+
+ 		 	</div>
+
+      	        <div id="form"> </div>
+
+        </div>
+ 	</div>
+ </body>
+ </html>
